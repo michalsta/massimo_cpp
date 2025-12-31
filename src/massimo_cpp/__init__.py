46194d326fd4b0f1515d4708573532ddb8891869
Mateusz Lacki <matteo.lacki@gmail.com>
2025-12-31 13:28:18 +0100

    missing str added

--- src/massimo_cpp/__init__.py
+++ src/massimo_cpp/__init__.py
@@ -1,9 +1,8 @@
 import cffi
+
 from IsoSpecPy.isoFFI import isoFFI
 
-isoFFI.ffi.dlopen(isoFFI.libpath, isoFFI.ffi.RTLD_GLOBAL | isoFFI.ffi.RTLD_NOLOAD)
+isoFFI.ffi.dlopen(str(isoFFI.libpath), isoFFI.ffi.RTLD_GLOBAL | isoFFI.ffi.RTLD_NOLOAD)
 
-from .massimo_cpp_ext import Massimize, ProblematicInput
-
-
-
+from .massimo_cpp_ext import Massimize
+from .massimo_cpp_ext import ProblematicInput
